287495dd90e7d6b3e91aa29e48161b705fa9f8c0
ajbrownp <ajbrownp@gmail.com>
2019-05-28 23:22:21 -0400

    dymanic class bidding

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.todos = [
+      {
+        id:1,
+        title:'Todo one',
+        completed: false
+      },
+      {
+        id:2,
+        title:'Todo two',
+        completed: false
+      },
+      {
+        id:3,
+        title:'Todo three',
+        completed: true
+      },
+      {
+        id:4,
+        title:'Todo four',
+        completed: true
+      }
+    ]
+  }
+
+}
